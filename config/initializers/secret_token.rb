baa186654ea63a822e7c54a6a06a00490923c58b
Hao Su <me@haosu.org>
2011-10-06 14:44:50 -0700

    rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SalesforceOAuth::Application.config.secret_token = '59b475244e80f1eb2ba46332946ed116c01207150914c5ebb83d5ca4a0f338bc0569e1df5917574bdc384b8cf0ccc8b40a1c756d8c9d28879074f16e4014c43d'
