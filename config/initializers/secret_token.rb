326db67630c5442cf105372f42777251bf5b60f1
haosu <me@haosu.org>
2012-01-14 18:57:00 -0800

    removed stuff

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SalesforceOAuth::Application.config.secret_token = '3987be9df7d45315c651696dbe99b961629371bcd55fa8768cea1d6529b74f223f58a1598d190c309797b70748bef183dd168f04b83d53772b10d3e46d1e10e3'
